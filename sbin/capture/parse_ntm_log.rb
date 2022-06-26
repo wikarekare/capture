0b5083da0463f08016da1a8126e0f5e029452ce3
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-26 17:00:58 +1200

    Ignore the YAML_safe rubocop for our own YAML file

--- sbin/capture/parse_ntm_log.rb
+++ sbin/capture/parse_ntm_log.rb
@@ -25,7 +25,7 @@ end
 @records = {}
 
 def line_ctl_from_yaml
-  File.open(CUSTOMER_NETWORKS, 'r') { |fd| @networks = YAML_safe.load(fd) }
+  File.open(CUSTOMER_NETWORKS, 'r') { |fd| @networks = YAML.load(fd) } # rubocop:disable Security/YAMLLoad # Not an issue with our own YAML file.
 end
 
 def insert_record(r)
